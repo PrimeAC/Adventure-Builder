8596a1c1f563e8e381593aa13e1fcca710400fcf
Daniel Reigada <daniel.reigada@gmail.com>
2017-03-07 21:28:53 +0000

    Create more tests for booking conflicts

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConflictMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConflictMethodTest.java
@@ -9,17 +9,18 @@ import org.junit.Test;
 public class BookingConflictMethodTest {
 	Booking booking;
 
+	private LocalDate bookingArrival = new LocalDate(2016, 12, 19);
+	private LocalDate bookingDeparture = new LocalDate(2016, 12, 24);
+
 	@Before
 	public void setUp() {
 		Hotel hotel = new Hotel("XPTO123", "Londres");
 
-		LocalDate arrival = new LocalDate(2016, 12, 19);
-		LocalDate departure = new LocalDate(2016, 12, 24);
-		this.booking = new Booking(hotel, arrival, departure);
+		this.booking = new Booking(hotel, bookingArrival, bookingDeparture);
 	}
 
 	@Test
-	public void noConflictBefore() {
+	public void noConflictBeforeLimitCase() {
 		LocalDate arrival = new LocalDate(2016, 12, 16);
 		LocalDate departure = new LocalDate(2016, 12, 19);
 
@@ -27,11 +28,59 @@ public class BookingConflictMethodTest {
 	}
 
 	@Test
-	public void noConflictAfter() {
+	public void noConflictBefore() {
+		LocalDate departure = bookingArrival.minusDays(1);
+		LocalDate arrival = departure.minusDays(3);
+
+		Assert.assertFalse(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void noConflictAfterLimitCase() {
 		LocalDate arrival = new LocalDate(2016, 12, 24);
 		LocalDate departure = new LocalDate(2016, 12, 30);
 
 		Assert.assertFalse(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void noConflictAfter() {
+		LocalDate arrival = bookingDeparture.plusDays(1);
+		LocalDate departure = arrival.plusDays(3);
+
+		Assert.assertFalse(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void conflictDepartureBetween() {
+		LocalDate arrival = bookingArrival.minusDays(1);
+		LocalDate departure = bookingDeparture.minusDays(1);
+
+		Assert.assertTrue(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void conflictArrivalBetween() {
+		LocalDate arrival = bookingArrival.plusDays(1);
+		LocalDate departure = bookingDeparture.plusDays(1);
+
+		Assert.assertTrue(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void conflictOldBetweenNew() {
+		LocalDate arrival = bookingArrival.minusDays(1);
+		LocalDate departure = bookingDeparture.plusDays(1);
+
+		Assert.assertTrue(this.booking.conflict(arrival, departure));
+	}
+
+	@Test
+	public void conflictNewBetweenOld() {
+		LocalDate arrival = bookingArrival.plusDays(1);
+		LocalDate departure = bookingDeparture.minusDays(1);
+
+		Assert.assertTrue(this.booking.conflict(arrival, departure));
 	}
 
 	@After
