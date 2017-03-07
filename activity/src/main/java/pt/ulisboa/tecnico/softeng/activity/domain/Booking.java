3836259706b93942dd83ad208d9bd7494f7df103
joaocarlos95 <joaocarlos95@gmail.com>
2017-03-07 16:01:33 +0000

    Complete tests for Booking's Constructor

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Booking.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Booking.java
@@ -1,14 +1,29 @@
 package pt.ulisboa.tecnico.softeng.activity.domain;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class Booking {
 	private static int counter = 0;
 
 	private final String reference;
 
 	public Booking(ActivityProvider provider, ActivityOffer offer) {
+		checkArguments(provider, offer);
+		checkAvailability(offer);
+
 		this.reference = provider.getCode() + Integer.toString(++Booking.counter);
 
 		offer.addBooking(this);
+	}
+
+	private void checkAvailability(ActivityOffer offer) {
+		if (!offer.hasVacancy())
+			throw new ActivityException();
+	}
+
+	private void checkArguments(ActivityProvider provider, ActivityOffer offer) {
+		if (provider == null || offer == null)
+			throw new ActivityException();
 	}
 
 	public String getReference() {
--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/BookingContructorMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/BookingContructorMethodTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
 
 public class BookingContructorMethodTest {
 	private ActivityProvider provider;
@@ -27,6 +28,82 @@ public class BookingContructorMethodTest {
 		Assert.assertTrue(booking.getReference().startsWith(this.provider.getCode()));
 		Assert.assertTrue(booking.getReference().length() > ActivityProvider.CODE_SIZE);
 		Assert.assertEquals(1, this.offer.getNumberOfBookings());
+	}
+
+	// Test to verify if Activity's capacity wasn't exceeded
+	@Test
+	public void testCapacity01() {
+		ActivityProvider activityProvider = new ActivityProvider("XtremY", "Motorcycling");
+		Activity activity = new Activity(activityProvider, "Trail", 18, 40, 3);
+
+		LocalDate begin = new LocalDate(2017, 03, 1);
+		LocalDate end = new LocalDate(2017, 03, 4);
+
+		ActivityOffer activityOffer = new ActivityOffer(activity, begin, end);
+
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+	}
+
+	// Test to verify if Activity's capacity wasn't exceeded
+	@Test
+	public void testCapacity02() {
+		ActivityProvider activityProvider = new ActivityProvider("XtremY", "Motorcycling");
+		Activity activity = new Activity(activityProvider, "Trail", 18, 40, 3);
+
+		LocalDate begin = new LocalDate(2017, 03, 1);
+		LocalDate end = new LocalDate(2017, 03, 4);
+
+		ActivityOffer activityOffer = new ActivityOffer(activity, begin, end);
+
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+	}
+
+	// Test to verify if Activity's capacity was exceeded
+	@Test (expected = ActivityException.class)
+	public void testCapacity03() {
+		ActivityProvider activityProvider = new ActivityProvider("XtremY", "Motorcycling");
+		Activity activity = new Activity(activityProvider, "Trail", 18, 40, 3);
+
+		LocalDate begin = new LocalDate(2017, 03, 1);
+		LocalDate end = new LocalDate(2017, 03, 4);
+
+		ActivityOffer activityOffer = new ActivityOffer(activity, begin, end);
+
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+		new Booking(activityProvider, activityOffer);
+	}
+
+	// Test to verify if Arguments of Booking's Constructor are valid
+	@Test (expected = ActivityException.class)
+	public void testArguments01() {
+		ActivityProvider activityProvider = new ActivityProvider("XtremY", "Motorcycling");
+		Activity activity = new Activity(activityProvider, "Trail", 18, 40, 1);
+
+		LocalDate begin1 = new LocalDate(2017, 03, 02);
+		LocalDate end1 = new LocalDate(2017, 03, 03);
+
+		ActivityOffer activityOffer = new ActivityOffer(activity, begin1, end1);
+
+		new Booking(null, activityOffer);
+	}
+
+	// Test to verify if Arguments of Booking's Constructor are valid
+	@Test (expected = ActivityException.class)
+	public void testArguments02() {
+		ActivityProvider activityProvider = new ActivityProvider("XtremY", "Motorcycling");
+
+		new Booking(activityProvider, null);
+	}
+
+	// Test to verify if Arguments of Booking's Constructor are valid
+	@Test (expected = ActivityException.class)
+	public void testArguments03() {
+		new Booking(null, null);
 	}
 
 	@After
