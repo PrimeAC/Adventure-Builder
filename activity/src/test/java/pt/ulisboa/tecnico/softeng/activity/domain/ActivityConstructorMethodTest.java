c705b5064c83dc0eb48dce4f738881f24e019cf2
joaocarlos95 <joaocarlos95@gmail.com>
2017-03-10 11:21:04 +0000

    Amend Exception's message from Activity and remove/add some tests

--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityConstructorMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityConstructorMethodTest.java
@@ -16,13 +16,13 @@ public class ActivityConstructorMethodTest {
 
 	@Test
 	public void success() {
-		Activity activity = new Activity(this.provider, "Bush Walking", 18, 80, 25);
+		Activity activity = new Activity(this.provider, "Bush Walking", 18, 99, 25);
 
 		Assert.assertTrue(activity.getCode().startsWith(this.provider.getCode()));
 		Assert.assertTrue(activity.getCode().length() > ActivityProvider.CODE_SIZE);
 		Assert.assertEquals("Bush Walking", activity.getName());
 		Assert.assertEquals(18, activity.getMinAge());
-		Assert.assertEquals(80, activity.getMaxAge());
+		Assert.assertEquals(99, activity.getMaxAge());
 		Assert.assertEquals(25, activity.getCapacity());
 		Assert.assertEquals(0, activity.getNumberOfOffers());
 		Assert.assertEquals(1, this.provider.getNumberOfActivities());
@@ -36,43 +36,35 @@ public class ActivityConstructorMethodTest {
 
     // Test to verify if both ages are between the limits
     @Test
+	public void testAge02() { new Activity(this.provider, "Bush Walking", 19, 80, 25); }
+
+    // Test to verify if both ages are between the limits
+	@Test
 	public void testAge03() {
-		new Activity(this.provider, "Bush Walking", 19, 80, 25);
-	}
-
-    // Test to verify if both ages are between the limits
-	@Test
-	public void testAge04() {
 		new Activity(this.provider, "Bush Walking", 20, 98, 25);
-	}
-
-    // Test to verify if both ages are between the limits
-	@Test
-	public void testAge05() {
-		new Activity(this.provider, "Bush Walking", 20, 99, 25);
 	}
 
     // Test to verify if maximum age is above the limit
 	@Test (expected = ActivityException.class)
-	public void testAge06() {
+	public void testAge04() {
 		new Activity(this.provider, "Bush Walking", 20, 100, 25);
 	}
 
     // Test to verify if ages are switched
 	@Test (expected = ActivityException.class)
-	public void testAge07() {
-		new Activity(this.provider, "Bush Walking", 80, 18, 25);
+	public void testAge05() {
+		new Activity(this.provider, "Bush Walking", 99, 18, 25);
 	}
 
     // Test to verify if both ages are between the limits
 	@Test
-	public void testAge08() {
+	public void testAge06() {
 		new Activity(this.provider, "Bush Walking", 18, 18, 25);
 	}
 
     // Test to verify if both ages are between the limits
 	@Test
-	public void testAge09() {
+	public void testAge07() {
 		new Activity(this.provider, "Bush Walking", 99, 99, 25);
 	}
 
@@ -85,8 +77,8 @@ public class ActivityConstructorMethodTest {
     public void testCapacity02() { new Activity(this.provider, "Bush Walking", 18, 80, 1); }
 
     // Test to verify if capacity is above the limit
-    @Test
-    public void testCapacity03() { new Activity(this.provider, "Bush Walking", 18, 80, 2); }
+    @Test (expected = ActivityException.class)
+    public void testCapacity03() { new Activity(this.provider, "Bush Walking", 18, 80, -1); }
 
     // Test to verify if ActivityProvider is null
     @Test (expected = ActivityException.class)
