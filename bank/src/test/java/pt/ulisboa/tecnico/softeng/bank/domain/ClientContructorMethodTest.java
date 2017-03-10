568824a9a5da6409fadd23936d794ac4b980f4c2
PrimeAC <afonso.caetano@outlook.com>
2017-03-10 11:29:05 +0000

    add Client constructor method tests

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/ClientContructorMethodTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/ClientContructorMethodTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 
 public class ClientContructorMethodTest {
 	Bank bank;
@@ -20,6 +21,26 @@ public class ClientContructorMethodTest {
 		Assert.assertEquals("António", client.getName());
 		Assert.assertTrue(client.getID().length() >= 1);
 		Assert.assertTrue(this.bank.hasClient(client));
+	}
+
+	@Test(expected = BankException.class)
+	public void testCheckNullBank() {
+		Client client = new Client(null, "Afonso");
+	}
+
+	@Test(expected = BankException.class)
+	public void testCheckNullName() {
+		Client client = new Client(this.bank, null);
+	}
+
+	@Test(expected = BankException.class)
+	public void testCheckEmptyName() {
+		Client client = new Client(this.bank, "");
+	}
+
+	@Test(expected = BankException.class)
+	public void testCheckAllWhitespacesName() {
+		Client client = new Client(this.bank, "    ");
 	}
 
 	@After
