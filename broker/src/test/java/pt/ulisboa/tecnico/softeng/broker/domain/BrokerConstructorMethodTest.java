0cb19989aca476adafa9efcbc9bb01d06db09a96
danielreis1 <ddreis88@gmail.com>
2017-03-12 16:44:37 +0000

    enhanced tests

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BrokerConstructorMethodTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BrokerConstructorMethodTest.java
@@ -11,65 +11,38 @@ public class BrokerConstructorMethodTest {
 	@Test
 	public void success() {
 		Broker broker = new Broker("BR01", "WeExplore");
-
 		Assert.assertEquals("BR01", broker.getCode());
 		Assert.assertEquals("WeExplore", broker.getName());
 		Assert.assertEquals(0, broker.getNumberOfAdventures());
 		Assert.assertTrue(Broker.brokers.contains(broker));
 	}
 
-	@Test
+	@Test(expected=BrokerException.class)
 	public void nullCode() {
-		try {
-			new Broker(null, "WeExplore");
-			Assert.fail();
-		} catch (BrokerException be) {
-			Assert.assertEquals(0, Broker.brokers.size());
-		}
+		new Broker(null, "WeExplore");
 	}
 
-	@Test
-	public void emptyCode() {
-		try {
-			new Broker("  ", "WeExplore");
-			Assert.fail();
-		} catch (BrokerException be) {
-			Assert.assertEquals(0, Broker.brokers.size());
-		}
-	}
-
-	@Test
-	public void uniqueCode() {
-		Broker broker = new Broker("BR01", "WeExplore");
-
-		try {
-			new Broker("BR01", "WeExploreX");
-			Assert.fail();
-		} catch (BrokerException be) {
-			Assert.assertEquals(1, Broker.brokers.size());
-			Assert.assertTrue(Broker.brokers.contains(broker));
-		}
-	}
-
-	@Test
+	@Test(expected=BrokerException.class)
 	public void nullName() {
-		try {
-			new Broker("BR01", null);
-			Assert.fail();
-		} catch (BrokerException be) {
-			Assert.assertEquals(0, Broker.brokers.size());
-		}
+		new Broker("BR01", null);
+	}
+	
+	@Test(expected=BrokerException.class)
+	public void emptyCode() {
+		new Broker("  ", "WeExplore");
+	}
+	
+	@Test(expected=BrokerException.class)
+	public void emptyName() {
+		new Broker("BR01", "    ");
 	}
 
-	@Test
-	public void emptyName() {
-		try {
-			new Broker("BR01", "    ");
-			Assert.fail();
-		} catch (BrokerException be) {
-			Assert.assertEquals(0, Broker.brokers.size());
-		}
+	@Test(expected=BrokerException.class)
+	public void uniqueCode() {
+		new Broker("BR01", "WeExplore");
+		new Broker("BR01", "WeExploreX");
 	}
+	
 
 	@After
 	public void tearDown() {
