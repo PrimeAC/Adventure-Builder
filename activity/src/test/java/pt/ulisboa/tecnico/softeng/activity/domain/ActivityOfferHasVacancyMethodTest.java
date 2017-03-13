488dd00982a46b1b6b924336e89293b49c26af97
MarianaCruz <mariana-r19@hotmail.com>
2017-03-13 15:39:05 +0000

    Complete ActivityOfferHasVacancyMethodTest tests

--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
@@ -6,13 +6,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class ActivityOfferHasVacancyMethodTest {
 	private ActivityOffer offer;
+	private ActivityProvider provider;
 
 	@Before
 	public void setUp() {
-		ActivityProvider provider = new ActivityProvider("XtremX", "ExtremeAdventure");
-		Activity activity = new Activity(provider, "Bush Walking", 18, 80, 3);
+		this.provider = new ActivityProvider("XtremX", "ExtremeAdventure");
+		Activity activity = new Activity(provider, "Bush Walking", 18, 80, 2);
 
 		LocalDate begin = LocalDate.now();
 		LocalDate end = begin.plusDays(2);
@@ -21,10 +24,18 @@ public class ActivityOfferHasVacancyMethodTest {
 	}
 
 	@Test
-	public void successZeroBookinks() {
+	public void sucessOneBooking() {
+		new Booking(this.provider,this.offer);
 		Assert.assertTrue(this.offer.hasVacancy());
 	}
-
+	
+	@Test
+	public void sucessTwoBooking() {
+		new Booking(this.provider,this.offer);
+		new Booking(this.provider,this.offer);
+		Assert.assertFalse(this.offer.hasVacancy());
+	}
+	
 	@After
 	public void tearDown() {
 		ActivityProvider.providers.clear();
