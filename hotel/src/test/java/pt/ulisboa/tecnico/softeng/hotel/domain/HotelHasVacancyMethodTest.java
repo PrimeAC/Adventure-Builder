ee94a233a98fe0d290d85b3790cc92b523c308f2
dimesq <d.mesquita285@gmail.com>
2017-03-17 09:40:21 +0000

    Add tests for hotel's hasVacancy method

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelHasVacancyMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelHasVacancyMethodTest.java
@@ -18,13 +18,79 @@ public class HotelHasVacancyMethodTest {
 	}
 
 	@Test
-	public void hasVacancy() {
+	public void roomAvailable() {
 		LocalDate arrival = new LocalDate(2016, 12, 19);
 		LocalDate departure = new LocalDate(2016, 12, 21);
 
 		Room room = this.hotel.hasVacancy(Type.DOUBLE, arrival, departure);
 
 		Assert.assertEquals("01", room.getNumber());
+	}
+
+	@Test
+	public void roomTypeNotAvailable() {
+
+		LocalDate arrival = new LocalDate(2016, 12, 18);
+		LocalDate departure = new LocalDate(2016, 12, 22);
+
+		Room room = this.hotel.hasVacancy(Type.SINGLE, arrival, departure);
+
+		Assert.assertEquals(null, room);
+	}
+
+	@Test
+	public void oneRoomNotAvailable() {
+		LocalDate booked_arrival = new LocalDate(2016, 12, 19);
+		LocalDate booked_departure = new LocalDate(2016, 12, 21);
+
+		this.hotel.reserveHotel(Type.DOUBLE, booked_arrival, booked_departure);
+
+		LocalDate arrival = new LocalDate(2016, 12, 18);
+		LocalDate departure = new LocalDate(2016, 12, 22);
+
+		Room room = this.hotel.hasVacancy(Type.DOUBLE, arrival, departure);
+
+		Assert.assertEquals(null, room);
+	}
+
+	@Test
+	public void twoRoomsAvailable() {
+		new Room(this.hotel, "02", Type.SINGLE);
+
+		LocalDate booked_arrival = new LocalDate(2016, 12, 19);
+		LocalDate booked_departure = new LocalDate(2016, 12, 21);
+
+		this.hotel.reserveHotel(Type.DOUBLE, booked_arrival, booked_departure);
+		this.hotel.reserveHotel(Type.SINGLE, booked_arrival, booked_departure);
+
+		LocalDate arrival = new LocalDate(2016, 12, 22);
+		LocalDate departure = new LocalDate(2016, 12, 24);
+
+		Room room1 = this.hotel.hasVacancy(Type.DOUBLE, arrival, departure);
+		Room room2 = this.hotel.hasVacancy(Type.SINGLE, arrival, departure);
+
+		Assert.assertEquals("01", room1.getNumber());
+		Assert.assertEquals("02", room2.getNumber());
+	}
+
+	@Test
+	public void twoRoomsNotAvailable() {
+		new Room(this.hotel, "02", Type.SINGLE);
+
+		LocalDate booked_arrival = new LocalDate(2016, 12, 19);
+		LocalDate booked_departure = new LocalDate(2016, 12, 21);
+
+		this.hotel.reserveHotel(Type.DOUBLE, booked_arrival, booked_departure);
+		this.hotel.reserveHotel(Type.SINGLE, booked_arrival, booked_departure);
+
+		LocalDate arrival = new LocalDate(2016, 12, 18);
+		LocalDate departure = new LocalDate(2016, 12, 22);
+
+		Room room1 = this.hotel.hasVacancy(Type.DOUBLE, arrival, departure);
+		Room room2 = this.hotel.hasVacancy(Type.SINGLE, arrival, departure);
+
+		Assert.assertEquals(null, room1);
+		Assert.assertEquals(null, room2);
 	}
 
 	@After
