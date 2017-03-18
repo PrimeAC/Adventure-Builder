9dc4e7486ceccbdc5fe63fd0342fbd3ff3ff5b91
danielreis <daniel.reis@tecnico.ulisboa.pt>
2017-03-18 11:58:00 +0000

    Bank getaccount test (#13)
    * more tests for bankHasAccount test class
    
    * added tests bankHasAccountMethodTest and fixed getAccount method
    
    * fixed a test
    
    * updated error messages
    
    * fixed error messages

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankHasAccountMethodTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/BankHasAccountMethodTest.java
@@ -5,13 +5,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
 public class BankHasAccountMethodTest {
 	Bank bank;
+
 	Client client;
 
 	@Before
 	public void setUp() {
-		this.bank = new Bank("Money", "BK01");
+		this.bank = new Bank("Money", "BK01"); 
 		this.client = new Client(this.bank, "António");
 	}
 
@@ -23,10 +26,40 @@ public class BankHasAccountMethodTest {
 
 		Assert.assertEquals(account, result);
 	}
-
+	
+	@Test(expected=BankException.class)
+	public void nullIBAN() {
+		this.bank.getAccount(null);
+	}
+	
+	@Test(expected=BankException.class)
+	public void IBANsizeCheck() {
+		//IBAN length >4
+		new Account(this.bank, this.client);
+		this.bank.getAccount("BK01");
+	}
+	
+	@Test(expected=BankException.class)
+	public void IBANblankStringCheck() {
+		new Account(this.bank, this.client);
+		this.bank.getAccount("     ");
+	}
+	
+	@Test(expected=BankException.class)
+	public void IBANnoAccount() {
+		this.bank.getAccount("BK01");
+	}
+	
+	@Test(expected=BankException.class)
+	public void IBANseveralAccountsWrongIBAN() {
+		Account a = new Account(this.bank, this.client);
+		Account a1 = new Account(this.bank, this.client);
+		this.bank.getAccount("BK0111232");
+	}
+	
 	@After
 	public void tearDown() {
 		Bank.banks.clear();
 	}
-
+	
 }
