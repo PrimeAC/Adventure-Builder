cc084107f631be7bbce7f60960f54e5426a3859a
MarianaCruz <mariana-r19@hotmail.com>
2017-03-23 23:24:40 +0000

    Refactor UndoState from Adventure #49

---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/UndoState.java
@@ -0,0 +1,53 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
+public class UndoState extends AdventureState {
+
+	@Override
+	public State getState() {
+		return State.UNDO;
+	}
+
+	@Override
+	public void process(Adventure adventure) {
+
+		if (adventure.cancelPayment()) {
+			try {
+				String paymentCancellation = BankInterface.cancelPayment(adventure.getPaymentConfirmation());
+				adventure.setPaymentCancellation(paymentCancellation);
+			} catch (HotelException | RemoteAccessException ex) {
+				adventure.setState(Adventure.State.UNDO);
+			}
+		}
+
+		if (adventure.cancelActivity()) {
+			try {
+				String activityCancellation = ActivityInterface.cancelReservation(adventure.getActivityConfirmation());
+				adventure.setActivityCancellation(activityCancellation);
+			} catch (HotelException | RemoteAccessException ex) {
+				adventure.setState(Adventure.State.UNDO);
+			}
+		}
+
+		if (adventure.cancelRoom()) {
+			try {
+				String roomCancellation = HotelInterface.cancelBooking(adventure.getRoomConfirmation());
+				adventure.setRoomCancellation(roomCancellation);
+			} catch (HotelException | RemoteAccessException ex) {
+				adventure.setState(Adventure.State.UNDO);
+			}
+		}
+
+		if (!adventure.cancelPayment() && !adventure.cancelActivity() && !adventure.cancelRoom()) {
+			adventure.setState(State.CANCELLED);
+		}
+
+	}
+
+}
