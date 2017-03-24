f4a5480486fed92b9f704b1dd1c722eb496adb0a
joaoplancha <joaoplancha@icloud.com>
2017-03-24 03:27:16 +0000

    refactor ProcessPaymentState from Adventure

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
@@ -156,7 +156,6 @@ public class Adventure {
 
 	public State getState() {
 		switch (this.oldState) {
-		case PROCESS_PAYMENT:
 		case RESERVE_ACTIVITY:
 		case BOOK_ROOM:
 		case UNDO:
@@ -174,7 +173,7 @@ public class Adventure {
 		this.oldState = state;
 		switch (state) {
 		case PROCESS_PAYMENT:
-			this.state = null;
+			this.state = new ProcessPaymentState();
 			break;
 		case RESERVE_ACTIVITY:
 			this.state = null;
@@ -203,20 +202,7 @@ public class Adventure {
 
 		switch (this.oldState) {
 		case PROCESS_PAYMENT:
-			try {
-				this.paymentConfirmation = BankInterface.processPayment(this.IBAN, this.amount);
-			} catch (BankException be) {
-				setState(State.CANCELLED);
-			} catch (RemoteAccessException rae) {
-				// increment number of errors
-				// if (number of errors == 3) {
-				// setState(State.CANCELLED);
-				// }
-				return;
-			}
-
-			setState(State.RESERVE_ACTIVITY);
-
+			this.state.process(this);
 			break;
 		case RESERVE_ACTIVITY:
 			try {
