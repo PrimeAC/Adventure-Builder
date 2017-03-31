b3b5dec70b338500057142ead244ecf0c7e64210
Daniel Reigada <daniel.reigada@gmail.com>
2017-03-31 09:10:30 +0100

    Implement bank cancelPayment

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Bank.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Bank.java
@@ -104,8 +104,13 @@ public class Bank {
 	}
 
 	public static String cancelPayment(String reference) {
-		// TODO implement
-		throw new BankException();
+		for (Bank bank : banks) {
+			Operation op = bank.getOperation(reference);
+			if (op != null) {
+				return op.getAccount().deposit(op.getValue());
+			}
+		}
+		throw new BankException("Operation with given reference not found");
 	}
 
 	public static BankOperationData getOperationData(String reference) {
