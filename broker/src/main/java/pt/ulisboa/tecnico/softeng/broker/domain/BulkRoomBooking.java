cd505fc6e8cf393e7424c1393706f3ed265eda19
dimesq <d.mesquita285@gmail.com>
2017-04-02 08:10:35 +0100

    Implment the getPreference method of the Bulk Room Booking class.

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBooking.java
@@ -1,8 +1,5 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.joda.time.LocalDate;
 
 import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
@@ -11,8 +8,14 @@ import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
 import pt.ulisboa.tecnico.softeng.hotel.dataobjects.RoomBookingData;
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BulkRoomBooking {
-	private final Set<String> references = new HashSet<>();
+	private static final int MAX_REMOTE_ERRORS = 20;
+	private static final int MAX_HOTEL_EXCEPTIONS = 3;
+
+	private Set<String> references = new HashSet<>();
 	private final int number;
 	private final LocalDate arrival;
 	private final LocalDate departure;
@@ -78,14 +81,15 @@ public class BulkRoomBooking {
 			RoomBookingData data = null;
 			try {
 				data = HotelInterface.getRoomBookingData(reference);
-				// this.numberOfRemoteErrors = 0;
+				this.numberOfRemoteErrors = 0;
 			} catch (HotelException he) {
-				// this.numberOfRemoteErrors = 0;
+				this.numberOfRemoteErrors = 0;
 			} catch (RemoteAccessException rae) {
-				// this.numberOfRemoteErrors++;
-				// if (this.numberOfRemoteErrors == MAX_REMOTE_ERRORS) {
-				// this.cancelled = true;
-				// }
+				this.numberOfRemoteErrors++;
+				if (this.numberOfRemoteErrors == MAX_REMOTE_ERRORS) {
+					this.cancelled = true;
+					break;
+				}
 			}
 
 			if (data != null && data.getRoomType().equals(type)) {
