d5fc0d8c80ea9e5f5ebc9995a33726934682d0ce
Daniel Reigada <daniel.reigada@gmail.com>
2017-04-12 20:56:22 +0100

    Make Account attributes persistent (closes #108)

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Account.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/domain/Account.java
@@ -5,19 +5,21 @@ import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 public class Account extends Account_Base {
 	private static int counter = 0;
 
-	private final String IBAN;
 	private final Client client;
-	private int balance;
 
 	public Account(Bank bank, Client client) {
 		checkArguments(bank, client);
-		
-		this.IBAN = bank.getCode() + Integer.toString(++Account.counter);
+
 		this.client = client;
-		this.balance = 0;
+		setIBAN(generateIBAN(bank));
+		setBalance(0);
 
 		bank.addAccount(this);
 		setBank(bank);
+	}
+
+	private String generateIBAN(Bank bank) {
+		return bank.getCode() + Integer.toString(++Account.counter);
 	}
 
 	private void checkArguments(Bank bank, Client client) {
@@ -31,35 +33,29 @@ public class Account extends Account_Base {
 
 	}
 
-	public String getIBAN() {
-		return this.IBAN;
-	}
 
 	public Client getClient() {
 		return this.client;
 	}
 
-	public int getBalance() {
-		return this.balance;
-	}
 
 	public String deposit(int amount) {
 		if (amount <= 0) {
 			throw new BankException();
 		}
 
-		this.balance = this.balance + amount;
+		setBalance(getBalance() + amount);
 
 		Operation operation = new Operation(Operation.Type.DEPOSIT, this, amount);
 		return operation.getReference();
 	}
 
 	public String withdraw(int amount) {
-		if (amount <= 0 || amount > this.balance) {
+		if (amount <= 0 || amount > getBalance()) {
 			throw new BankException();
 		}
 
-		this.balance = this.balance - amount;
+		setBalance(getBalance() - amount);
 
 		return new Operation(Operation.Type.WITHDRAW, this, amount).getReference();
 	}
