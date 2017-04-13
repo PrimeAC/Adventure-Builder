7eca60e99148e565e7a95627f8267941acba7114
PrimeAC <afonso.caetano@outlook.com>
2017-04-14 00:01:37 +0100

    Add persistence to Hotel class attributes (closes #98)

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Hotel.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Hotel.java
@@ -16,15 +16,13 @@ public class Hotel extends Hotel_Base {
 
 	static final int CODE_SIZE = 7;
 
-	private final String code;
-	private final String name;
 	private final Set<Room> rooms = new HashSet<>();
 
 	public Hotel(String code, String name) {
 		checkArguments(code, name);
 
-		this.code = code;
-		this.name = name;
+		setCode(code);
+		setName(name);
 
 		FenixFramework.getDomainRoot().addHotel(this);
 	}
@@ -71,14 +69,6 @@ public class Hotel extends Hotel_Base {
 			}
 		}
 		return availableRooms;
-	}
-
-	public String getCode() {
-		return this.code;
-	}
-
-	public String getName() {
-		return this.name;
 	}
 
 	void addRoom(Room room) {
