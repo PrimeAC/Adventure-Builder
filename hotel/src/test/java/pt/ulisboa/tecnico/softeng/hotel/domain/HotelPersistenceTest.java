53149cfb4bd008e0c81dc4f0a2a142f09554608e
PrimeAC <afonso.caetano@outlook.com>
2017-04-19 14:47:11 +0100

    reformat test class

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelPersistenceTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelPersistenceTest.java
@@ -14,8 +14,6 @@ public class HotelPersistenceTest {
 	private static final String HOTEL_NAME1 = "Londres";
 	private static final String HOTEL_CODE2 = "XPTO456";
 	private static final String HOTEL_NAME2 = "Paris";
-	private static final String HOTEL_CODE3 = "XPTO789";
-	private static final String HOTEL_NAME3 = "New York";
 
 	@Test
 	public void success() {
@@ -23,68 +21,25 @@ public class HotelPersistenceTest {
 		atomicAssert();
 	}
 
-	@Test
-	public void test2Hotels(){
-		atomicProcess2();
-		atomicAssert2();
-	}
-
-	@Test
-	public void testDeletionOfHotel(){
-		atomicProcess3();
-		atomicAssert3();
-	}
-
 	@Atomic(mode = Atomic.TxMode.WRITE)
 	public void atomicProcess() {
 		new Hotel(HOTEL_CODE1, HOTEL_NAME1);
-	}
-
-	@Atomic(mode = Atomic.TxMode.READ)
-	public void atomicAssert() {
-		Hotel hotel = Hotel.getHotelByCode(HOTEL_CODE1);
-
-		assertNotNull(hotel);
-		assertEquals(1, FenixFramework.getDomainRoot().getHotelSet().size());
-		assertEquals(HOTEL_NAME1, hotel.getName());
-	}
-
-	@Atomic(mode = Atomic.TxMode.WRITE)
-	public void atomicProcess2() {
-		new Hotel(HOTEL_CODE1,HOTEL_NAME1 );
 		new Hotel(HOTEL_CODE2,HOTEL_NAME2);
 	}
 
 	@Atomic(mode = Atomic.TxMode.READ)
-	public void atomicAssert2() {
+	public void atomicAssert() {
 		Hotel hotel1 = Hotel.getHotelByCode(HOTEL_CODE1);
 		Hotel hotel2 = Hotel.getHotelByCode(HOTEL_CODE2);
+		Hotel hotelNull = Hotel.getHotelByCode("XPTO000");
+
+		assertNotNull(hotel1);
+		assertNotNull(hotel2);
+		assertNull(hotelNull);
 
 		assertEquals(2, FenixFramework.getDomainRoot().getHotelSet().size());
 		assertEquals(HOTEL_NAME1, hotel1.getName());
 		assertEquals(HOTEL_NAME2, hotel2.getName());
-
-	}
-
-	@Atomic(mode = Atomic.TxMode.WRITE)
-	public void atomicProcess3() {
-		new Hotel(HOTEL_CODE1,HOTEL_NAME1);
-		Hotel hotel = new Hotel(HOTEL_CODE2,HOTEL_NAME2);
-		new Hotel(HOTEL_CODE3, HOTEL_NAME3);
-
-		hotel.delete();
-	}
-
-	@Atomic(mode = Atomic.TxMode.READ)
-	public void atomicAssert3() {
-		Hotel hotel1 = Hotel.getHotelByCode(HOTEL_CODE1);
-		Hotel nullHotel = Hotel.getHotelByCode(HOTEL_CODE2);
-		Hotel hotel2 = Hotel.getHotelByCode(HOTEL_CODE3);
-
-		assertNull(nullHotel);
-		assertEquals(2, FenixFramework.getDomainRoot().getHotelSet().size());
-		assertEquals(HOTEL_NAME1, hotel1.getName());
-		assertEquals(HOTEL_NAME3, hotel2.getName());
 
 	}
 
