6756a3a6bef224678125a9d7475779cfa68408ac
João Freitas <joaotavaresfreitas@hotmail.com>
2017-04-21 15:26:31 +0100

    add persistence to activity (#135)
    * close #117 activity persistence
    
    * remove .gitignore file in directory add code attribute to database
    
    * Fix requests

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
@@ -13,23 +13,21 @@ public class Activity extends Activity_Base{
 
 	private static int counter = 0;
 
-	private final String name;
 	private final String code;
-	private final int minAge;
-	private final int maxAge;
-	private final int capacity;
+	
 	private final Set<ActivityOffer> offers = new HashSet<>();
 
 	public Activity(ActivityProvider provider, String name, int minAge, int maxAge, int capacity) {
 		checkArguments(provider, name, minAge, maxAge, capacity);
 
-		this.code = provider.getCode() + Integer.toString(++Activity.counter);
-		this.name = name;
-		this.minAge = minAge;
-		this.maxAge = maxAge;
-		this.capacity = capacity;
+		code = provider.getCode() + Integer.toString(++Activity.counter);
+		setCode(code);
+		setName(name);
+		setMinAge(minAge);
+		setMaxAge(maxAge);
+		setCapacity(capacity);
 
-		provider.addActivity(this);
+		setActivityProvider(provider);
 	}
 
 	private void checkArguments(ActivityProvider provider, String name, int minAge, int maxAge, int capacity) {
@@ -46,25 +44,11 @@ public class Activity extends Activity_Base{
 		}
 
 	}
-
-	String getName() {
-		return this.name;
-	}
-
-	String getCode() {
-		return this.code;
-	}
-
-	int getMinAge() {
-		return this.minAge;
-	}
-
-	int getMaxAge() {
-		return this.maxAge;
-	}
-
-	int getCapacity() {
-		return this.capacity;
+	
+	public void delete() {
+		getActivityOfferSet().forEach(ActivityOffer::delete);
+		setActivityProvider(null);
+		deleteDomainObject();
 	}
 
 	int getNumberOfOffers() {
@@ -86,7 +70,7 @@ public class Activity extends Activity_Base{
 	}
 
 	boolean matchAge(int age) {
-		return age >= this.minAge && age <= this.maxAge;
+		return age >= getMinAge() && age <= getMaxAge();
 	}
 
 	public Booking getBooking(String reference) {
