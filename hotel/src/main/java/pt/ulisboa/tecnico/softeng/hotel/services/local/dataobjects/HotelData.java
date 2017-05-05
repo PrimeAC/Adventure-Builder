30e01f3fcdffbfd477c5c1d5bcce710c433b24f2
MarianaCruz <mariana-r19@hotmail.com>
2017-05-05 14:56:51 +0100

    Create new Hotel Data Objects (closes #154)

---
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/local/dataobjects/HotelData.java
@@ -0,0 +1,44 @@
+package pt.ulisboa.tecnico.softeng.hotel.services.local.dataobjects;
+
+import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel;
+
+public class HotelData {
+	public static enum CopyDepth {
+		SHALLOW, BOOKINGS, ROOMS
+	};
+
+	private String name;
+	private String code;
+
+	public HotelData() {
+	}
+
+	public HotelData(Hotel hotel, CopyDepth depth) {
+		this.name = hotel.getName();
+		this.code = hotel.getCode();
+
+		switch (depth) {
+		case SHALLOW:
+			break;
+		default:
+			break;
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
