4360de61df0ab8df6f8af0f80d106d8b73b6392b
Daniel Reigada <daniel.reigada@gmail.com>
2017-05-06 21:31:01 +0100

    List offers reservations

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/dataobjects/ActivityOfferData.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/dataobjects/ActivityOfferData.java
@@ -1,19 +1,38 @@
 package pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects;
 
 import org.joda.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
 import pt.ulisboa.tecnico.softeng.activity.domain.ActivityOffer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActivityOfferData {
+	public enum CopyDepth {
+		SHALLOW, BOOKINGS
+	}
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate begin, end;
 	private int capacity;
+	private List<ActivityBookingData> bookings = new ArrayList<>();
 
 	public ActivityOfferData() {
 	}
 
-	public ActivityOfferData(ActivityOffer offer) {
+	public ActivityOfferData(ActivityOffer offer, CopyDepth depth) {
 		begin = offer.getBegin();
 		end = offer.getEnd();
 		capacity = offer.getCapacity();
+
+		switch (depth) {
+			case BOOKINGS:
+				offer.getBookingSet().forEach(booking -> bookings.add(new ActivityBookingData(booking)));
+				break;
+			case SHALLOW:
+			default:
+				break;
+		}
 	}
 
 	public LocalDate getBegin() {
@@ -38,5 +57,13 @@ public class ActivityOfferData {
 
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
+	}
+
+	public List<ActivityBookingData> getBookings() {
+		return bookings;
+	}
+
+	public void setBookings(List<ActivityBookingData> bookings) {
+		this.bookings = bookings;
 	}
 }
